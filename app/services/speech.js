1acc4cfb06a77c4624b1e8c0adf15e8997a45803
chikoo0907 <rajbhuran03@gmail.com>
2026-03-31 12:29:28 +0530

    commit

--- app/services/speech.js
+++
@@ -1,6 +0,0 @@
-import * as Speech from "expo-speech";
-
-
-export function speak(text, lang = "en") {
-Speech.speak(text, { language: lang });
-}
--- prisma/migrations/20260218142809_store_document_in_db/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Document" ADD COLUMN     "data" BYTEA,
-ALTER COLUMN "path" DROP NOT NULL;
